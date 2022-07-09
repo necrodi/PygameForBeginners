0bd5b4802e3dc36c87aba2e79b646719197fdcbc
Jan Eckerlein <eckerlein.jan@gmail.com>
2022-07-10 00:50:00 +0200

    cleaned up Constants

--- main.py
+++ main.py
@@ -2,28 +2,36 @@ from tkinter import Y
 import pygame
 import os
 
-from refference import RED, SPACESHIP_HEIGHT, SPACESHIP_WIDTH
-
-SIZE = [900,500]
-WIN = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("First Game")
-
-WHITE = (255,255,255)
-BACKGROUND = (10, 10, 10)
-
+#modifiable CONSTANTS
+SIZE = WIDTH, HEIGHT = 900,500
+BORDER_WIDTH = 10
 FPS = 60
 VEL = 5
 
+#immutable CONSTANTS
+BORDER_START = WIDTH/2 - BORDER_WIDTH/2
+BORDER_END = WIDTH/2 + BORDER_WIDTH/2
 SPACESHIP_SIZE = SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
+#COLORS
+BORDER_COLOR = (255, 255, 255)
+BACKGROUND = (10, 10, 10)
+
+#WINDOW Objects
+BORDER = pygame.Rect(BORDER_START, 0, BORDER_WIDTH, HEIGHT)
+WIN = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("First Game")
+
+#loading Player Images
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
-
 YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, SPACESHIP_SIZE), 90)
 RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, SPACESHIP_SIZE), -90)
 
+
 def draw_window(yellow, red):
     WIN.fill(BACKGROUND)
+    pygame.draw.rect(WIN, BORDER_COLOR, BORDER)
     WIN.blit(YELLOW_SPACESHIP_IMAGE, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP_IMAGE, (red.x, red.y))
     pygame.display.update()
